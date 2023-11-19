e5983138f0658489fc2e8ec467b375fd0f38d92d
euskate <euskate07@gmail.com>
2023-11-19 09:33:57 +0900

    register test

---
+++ src/components/Login.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Login() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+
+    // const url = "http://localhost:8080/api/member";
+    // await axios
+    //   .post(url, formData)
+    //   .then((response) => {
+    //     if (response.status === 200) {
+    //       console.log(response);
+    //       console.log("create memeber id", response.data);
+    //     }
+    //   })
+    //   .catch((error) => console.log(error));
+    // Or you can work with it as a plain object:
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+  };
+  return (
+    <div>
+      <h2>Login</h2>
+      <form method="post" onSubmit={handleSubmit}>
+        <div>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="email을 입력하세요."
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="비밀번호를 입력하세요"
+          />
+        </div>
+        <button>로그인</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
